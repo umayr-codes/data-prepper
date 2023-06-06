bdf30322a83e1bff8f427f639d22359bf3212cc2
umairofficial <umairhusain1010@gmail.com>
2023-06-06 15:11:51 +0530

    -Support for Sink Codecs Signed-off-by: umairofficial <umairhusain1010@gmail.com>

--- data-prepper-api/src/main/java/org/opensearch/dataprepper/model/codec/OutputCodec.java
+++ data-prepper-api/src/main/java/org/opensearch/dataprepper/model/codec/OutputCodec.java
@@ -6,6 +6,7 @@
 package org.opensearch.dataprepper.model.codec;
 
 import org.opensearch.dataprepper.model.event.Event;
+import org.opensearch.dataprepper.model.sink.Sink;
 
 import java.io.IOException;
 import java.io.OutputStream;
@@ -13,29 +14,36 @@ import java.io.OutputStream;
 public interface OutputCodec {
 
     /**
-     * Sink will initiate the start() method of OutputCodec and do initialization of stream
+     * this method get called from {@link Sink} to do initial wrapping in {@link OutputStream}
+     * Implementors should do initial wrapping according to the implementation
      *
-     * @param outputStream
+     * @param outputStream outputStream param for wrapping
+     * @throws IOException throws IOException when invalid input is received or not able to create wrapping
      */
     void start(OutputStream outputStream) throws IOException;
 
     /**
-     * Sink will call complete() method of OutputCodec and do the final wrapping.
+     * this method get called from {@link Sink} to do final wrapping in {@link OutputStream}
+     * Implementors should do final wrapping according to the implementation
      *
-     * @param outputStream
+     * @param outputStream outputStream param for wrapping
+     * @throws IOException throws IOException when invalid input is received or not able to create wrapping
      */
     void complete(OutputStream outputStream) throws IOException;
 
+
     /**
-     * Sink will call writeEvent() - to write schema to output Stream
+     * this method get called from {@link Sink} to write event in {@link OutputStream}
+     * Implementors should do get data from event and write to the {@link OutputStream}
      *
-     * @param event
-     * @param outputStream
+     * @param event        event Record event
+     * @param outputStream outputStream param to hold the event data
+     * @throws IOException throws IOException when not able to write data to {@link OutputStream}
      */
     void writeEvent(Event event, OutputStream outputStream) throws IOException;
 
     /**
-     * used to get extesion of file
+     * used to get extension of file
      *
      * @return String
      */
